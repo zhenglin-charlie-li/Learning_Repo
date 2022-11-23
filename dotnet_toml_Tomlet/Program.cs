46f5fb89a809cfe39513bd89db261e45231fde9d
zhenglinli <1125806272@qq.com>
2022-11-23 12:20:12 +0800

    generic host

---
+++ dotnet_toml_Tomlet/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Tomlet;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string myTomlString =
+                @"
+Rule1 = '*,EW,*,ErrorAndWarning'
+Rule2 = '*,I,*,Info'
+MaxFiles = 10
+[ErrorAndWarning]
+MaxFileSize = 10000000
+FileNameBase = 'Local\ErrorAndWarning'
+BufferSize = 10000";
+            TomlParser parser = new TomlParser();
+            Tomlet.Models.TomlDocument document = parser.Parse(myTomlString);
+            Console.WriteLine("document");
+
+            int someInt = document.GetInteger("MaxFiles");
+            string someString = document.GetString("Rule1");
+            Console.WriteLine(someInt);
+            Console.WriteLine(someString);
+
+            int subTableInt = document.GetSubTable("ErrorAndWarning").GetInteger("BufferSize");
+            string subTableString = document.GetSubTable("ErrorAndWarning").GetString("FileNameBase");
+            Console.WriteLine(subTableInt);
+            Console.WriteLine(subTableString);
+        }
+    }
+}
